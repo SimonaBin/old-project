8169784db7bec97eb962f6d0c6b02811e94ea4ea
simona <simona>
2018-10-14 21:35:37 +0300

    add  main class

---
+++ guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicApplication.class, args);
+	}
+}
